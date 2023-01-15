525ac324f891c5190e9c941dadb2a89a31c1a5c3
NamUit789 <NamUit789@gmail.com>
2023-01-15 20:10:05 +0700

    add user model

---
+++ Masset/Data/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Masset.Data.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Masset/Data/Enums/UserTypeEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Masset.Data.Enums
+{
+    public enum UserTypeEnum
+    {
+        Admin = 1,
+        Employee = 2
+    }
+}
